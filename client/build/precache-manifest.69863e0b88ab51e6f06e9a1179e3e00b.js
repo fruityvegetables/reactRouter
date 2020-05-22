65fbdcc9b7bd512e386739fd619b6043245754f8
fruityvegetables <gg.troyer@gmail.com>
2020-05-21 20:36:39 -0500

    removed gitignore files

---
+++ client/build/precache-manifest.69863e0b88ab51e6f06e9a1179e3e00b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0614ed908b556714e2be",
+    "url": "/static/css/main.29b34297.chunk.css"
+  },
+  {
+    "revision": "0614ed908b556714e2be",
+    "url": "/static/js/main.71b9146b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "bc268f8875d695e67052eeaa38c5196f",
+    "url": "/index.html"
+  }
+];
